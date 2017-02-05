88bcbfae35fdfc560ef3e6837d3d1672806339e7
nindraji <nindraji@lakeheadu.ca>
2017-02-05 14:19:06 -0500

    Update Crawlerissuereport.py

--- Crawlerissuereport.py
+++ Crawlerissuereport.py
@@ -10,8 +10,9 @@ while i<len(crawlreport):
    url = "https://issues.apache.org/jira/browse/CAMEL-" + crawlreport[i]+ "10597"
    htmlfile =urllib.urlopen(url)
    htmltext=htmlfile.read()
-   regex = '<span title="2.18.0 planned for after summer 2016'+crawlreport[i]+'">2.18.0</span>'                                                                              
+   regex = '<span title="2.18.0 report'+crawlreport[i]+'">2.18.0</span>'                                                                              
    pattern = re.compile(regex)
    details = re.findall(pattern,htmltext)
    print "The",crawlreport[i]," is " ,details 
    i+=1
+
